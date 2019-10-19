a56a6822bcc023bfb89d5355a61506806ea6c4fe
root <you@example.com>
2019-10-19 21:53:21 +0800

    update

--- day7/01_socket_server.go
+++ day7/01_socket_server.go
@@ -13,19 +13,22 @@ func main() {
 		return
 	}
 	defer listener.Close()
-
+	fmt.Println("服务器启动")
 	// 阻塞等待用户连接
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
 
-	// 接收用户请求
-	buf := make([]byte, 1024)
-	n, err1 := conn.Read(buf)
-	if err1 != nil {
-		fmt.Println("Error:", err1)
+		// 接收用户请求
+		buf := make([]byte, 1024)
+		n, err1 := conn.Read(buf)
+		if err1 != nil {
+			fmt.Println("Error:", err1)
+		}
+		fmt.Println(string(buf[:n]))
+		defer conn.Close()
 	}
-	fmt.Println(string(buf[:n]))
-	defer conn.Close()
 }
