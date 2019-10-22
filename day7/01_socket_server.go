692a91d9d3edc804778174ac3e8ae1d49b0375fa
root <you@example.com>
2019-10-22 15:42:20 +0800

    update

--- day7/01_socket_server.go
+++ day7/01_socket_server.go
@@ -3,7 +3,34 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
+
+func HandleConn(conn net.Conn) {
+	// 函数调用结束，关闭连接
+	defer conn.Close()
+
+	// 获取客户端网络地址信息
+	addr := conn.RemoteAddr().String()
+	fmt.Printf("[%s]: addr conncet sucessful\n", addr)
+	for {
+		// 读取用户数据
+		buf := make([]byte, 2048)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Printf("[%s]: %s\n", addr, string(buf[:n]))
+		if "exit" == string(buf[:n-2]) {
+			fmt.Printf("[%s]: exit\n", addr)
+			return
+		}
+
+		// 把数据转换成大写
+		conn.Write([]byte(strings.ToUpper(string(buf[:n]))))
+	}
+}
 
 func main() {
 	// 监听
@@ -14,6 +41,7 @@ func main() {
 	}
 	defer listener.Close()
 	fmt.Println("服务器启动")
+
 	// 阻塞等待用户连接
 	for {
 		conn, err := listener.Accept()
@@ -22,13 +50,7 @@ func main() {
 			continue
 		}
 
-		// 接收用户请求
-		buf := make([]byte, 1024)
-		n, err1 := conn.Read(buf)
-		if err1 != nil {
-			fmt.Println("Error:", err1)
-		}
-		fmt.Println(string(buf[:n]))
-		defer conn.Close()
+		// 开启协程处理用户请求
+		go HandleConn(conn)
 	}
 }
--- day7/02_socket_client.go
+++ day7/02_socket_client.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -12,6 +14,30 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
+	fmt.Println("服务器连接成功")
 	defer conn.Close()
-	conn.Write([]byte("hello world"))
+
+	// 输入内容，发送至服务器
+	go func() {
+		str := make([]byte, 2048)
+		for {
+			n, err := os.Stdin.Read(str)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			conn.Write(str[:n])
+		}
+	}()
+
+	// 接收服务器回复数据
+	buf := make([]byte, 2048)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(string(buf[:n]))
+	}
 }
